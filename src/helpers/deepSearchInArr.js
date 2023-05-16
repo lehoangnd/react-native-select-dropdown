ea99a053498bf2fc2568589a62d146ea5e70dc09
Lê Hoàng <80310280+lehoangnd@users.noreply.github.com>
2023-05-16 09:45:55 +0700

    Update deepSearchInArr.js

--- src/helpers/deepSearchInArr.js
+++ src/helpers/deepSearchInArr.js
@@ -1,3 +1,20 @@
+const nonAccentVietnamese = str => {
+  if (!str) return '';
+  let result = str.trim();
+  result = result.toLowerCase();
+  result = result.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+  result = result.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+  result = result.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+  result = result.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+  result = result.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+  result = result.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+  result = result.replace(/đ/g, 'd');
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  result = result.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ''); // Huyền sắc hỏi ngã nặng
+  result = result.replace(/\u02C6|\u0306|\u031B/g, ''); // Â, Ê, Ă, Ơ, Ư
+  return result;
+};
+
 const contains = (item, searchTxt) => {
   // item is an object
   if (typeof item == 'object' && item != null) {
@@ -10,8 +27,8 @@ const contains = (item, searchTxt) => {
   }
   // string, number or boolean
   if (typeof item != 'object' && item != null && item != undefined) {
-    const itemStringfied = item.toString().toLowerCase();
-    const searchTxtStringfied = searchTxt.toString().toLowerCase();
+    const itemStringfied = nonAccentVietnamese(item.toString().toLowerCase());
+    const searchTxtStringfied = nonAccentVietnamese(searchTxt.toString().toLowerCase());
     if (itemStringfied.includes(searchTxtStringfied)) {
       return true;
     }
